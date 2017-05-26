7e4a5f94b4aeb25315577beba30d9e27c9ee1184
gongruike <gongruike@gongruikedeMacBook-Pro.local>
2017-05-26 16:30:07 +0800

    remove unnecessary info

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -18,14 +18,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var refreshBtn: UIButton!
     
-    let authManager: RKOAuthManager = RKOAuthManager(clientID: "mobile.tiup.cn")
+    let authManager: RKOAuthManager = RKOAuthManager(clientID: "")
     
     var credential: RKCredential?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        authManager.clientSecret = "6f52db21ce5c52c180f04b298d974f779d854147173658b5ba51b6db950028f8"
+        authManager.clientSecret = ""
         authManager.useHTTPBasicAuthentication = true
     }
     
@@ -35,7 +35,7 @@ class ViewController: UIViewController {
             return
         }
         
-        let url = "https://test.tiup.cn/oauth2/token"
+        let url = ""
         authManager.authenticate(url, username: usernameField.text!, password: passwordField.text!, scope: "all") { [weak self] (result) in
             switch result {
             case .success(let value):
@@ -50,7 +50,7 @@ class ViewController: UIViewController {
         
         guard let refreshToken = credential?.refreshToken else { return }
         
-        let url = "https://test.tiup.cn/oauth2/token"
+        let url = ""
         authManager.authenticate(url, refreshToken: refreshToken) { [weak self] (result) in
             switch result {
             case .success(let value):
